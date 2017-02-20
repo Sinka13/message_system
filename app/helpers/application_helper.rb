907d101d57f407854e484590b356327ad61f05fb
Sinka <sinka13666@gmail.com>
2017-02-20 23:25:07 +0200

    notification alpha added, refresh start

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def set_resipient(conversation)
-    current_user == conversation.resipient ? conversation.sender : conversation.resipient
+  def set_resipient(chat)
+    current_user == chat.resipient ? chat.sender : chat.resipient
+  end
+#TODO temporary solution   @chats.joins(:messages).group('id').count
+  def new_messages(chats, user)
+    count = 0
+    chats.each do |chat|
+      count += chat.messages.unread(user).count
+    end
+    return count
   end
 end
