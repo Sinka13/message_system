b1e1d17c26888a1cdc9fb623cf1ae811e22a7987
Sinka <sinka13666@gmail.com>
2017-02-20 23:46:24 +0200

    fixed sql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,5 @@ module ApplicationHelper
   def set_resipient(chat)
     current_user == chat.resipient ? chat.sender : chat.resipient
   end
-#TODO temporary solution   @chats.joins(:messages).group('id').count
-  def new_messages(chats, user)
-    count = 0
-    chats.each do |chat|
-      count += chat.messages.unread(user).count
-    end
-    return count
-  end
+
 end
