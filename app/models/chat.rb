018f5a48658b54dfa08cd095638e7ffd575aa6f3
Sinka <sinka13666@gmail.com>
2017-02-21 11:51:52 +0200

    UI on front fix, scope added

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,6 +3,7 @@ class Chat < ApplicationRecord
   belongs_to :resipient, foreign_key: :resipient_id, class_name: 'User'
 
   has_many :messages, dependent: :destroy
+  scope :new_messages, -> (user) { where.not("messages.user_id = ?",  user.id).where("messages.is_new =?", true)}
 
   scope :involving, -> (user) do
     where("chats.sender_id =? OR chats.resipient_id =?",user.id,user.id)
